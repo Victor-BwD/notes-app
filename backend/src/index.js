408647fdf028f7dde9f42908ddf5398e197917a7
Victor B. Dornelles <victor.bogdanowdornelles@gmail.com>
2023-02-06 20:52:12 -0300

    Installing cors, backend finalized

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const routes = require('./routes');
+const cors = require('cors');
 
 const app = express();
 require('./config/dbConfig');
 
 
-
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
