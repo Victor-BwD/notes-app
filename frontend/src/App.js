bb8ccee5518b2817082407d63bf74455c24a6321
Victor B. Dornelles <victor.bogdanowdornelles@gmail.com>
2023-02-07 16:12:24 -0300

    Html skeleton, global.css added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,26 @@
+import './global.css'
+
 
 function App() {
   return (
-    <h1>Hello world</h1>
+    <div id="app">
+      <aside>
+        <strong>Caderno de Notas</strong>
+        <form>
+          <div className="input-block">
+              <label for="title">Título da anotação</label>
+              <input></input>
+          </div>
+          <div className="input block">
+              <label for="nota">Anotações</label>
+              <textarea></textarea>
+          </div>
+
+          <button type="submit">Salvar</button>
+        </form>
+      </aside>
+      <main></main>
+    </div>
   );
 }
 
