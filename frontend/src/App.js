fa8142c6603c97389afbe96ee3d5fa9c15391b31
Victor B. Dornelles <victor.bogdanowdornelles@gmail.com>
2023-02-09 22:08:25 -0300

    Card html

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import './global.css'
 import './sidebar.css'
+import './app.css'
 
 function App() {
   return (
@@ -19,7 +20,21 @@ function App() {
           <button type="submit">Salvar</button>
         </form>
       </aside>
-      <main></main>
+      <main>
+        <ul>
+          <li className='notepad-infos'>
+            <div>
+              <strong>Fazer compras</strong>
+              <div>
+                x
+              </div>
+            </div>
+
+            <textarea>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</textarea>
+            <span>!</span>
+          </li>
+        </ul>
+      </main>
     </div>
   );
 }
