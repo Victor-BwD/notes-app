ee6cd0e143515d97965a16e5d08a577d12c54784
Victor B. Dornelles <victor.bogdanowdornelles@gmail.com>
2023-02-10 22:58:24 -0300

    Componetization

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,9 @@
-import './global.css'
-import './sidebar.css'
-import './app.css'
-import './main.css'
+import './global.css';
+import './sidebar.css';
+import './app.css';
+import './main.css';
+
+import Notes from './components/notes';
 
 function App() {
   return (
@@ -22,19 +24,7 @@ function App() {
         </form>
       </aside>
       <main>
-        <ul>
-          <li className='notepad-infos'>
-            <div>
-              <strong>Fazer compras</strong>
-              <div>
-                x
-              </div>
-            </div>
-
-            <textarea>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</textarea>
-            <span>!</span>
-          </li>
-        </ul>
+          <Notes />
       </main>
     </div>
   );
---
+++ frontend/src/components/notes/index.js
@@ -0,0 +1,21 @@
+
+
+export default function Notes() {
+    return (
+        <>
+        <ul>
+          <li className='notepad-infos'>
+            <div>
+              <strong>Fazer compras</strong>
+              <div>
+                x
+              </div>
+            </div>
+
+            <textarea>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</textarea>
+            <span>!</span>
+          </li>
+        </ul>
+        </>
+    );
+}
