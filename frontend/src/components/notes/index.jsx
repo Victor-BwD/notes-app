640d086c144f41774251a661c11fff125e7b4a30
Victor B. Dornelles <victor.bogdanowdornelles@gmail.com>
2023-03-22 23:34:17 -0300

    Save when click outside box

--- frontend/src/components/notes/index.jsx
+++ frontend/src/components/notes/index.jsx
@@ -1,8 +1,20 @@
 import { AiTwotoneDelete, AiOutlineExclamationCircle } from "react-icons/ai";
 import './styles.css'
 import './styles-priority.css'
+import { useState } from "react";
+import api from '../services/api'
 
 export default function Notes({ data }) {
+  const [changedNote, setChangedNote] = useState('');
+
+  async function handleSave(e, notes) {
+    if(changedNote && changedNote !== notes) {
+      await api.post(`/contents/${data._id}`,{
+        notes: changedNote,
+      });
+    }
+  }
+
     return (
         <>
         <ul>
@@ -14,7 +26,13 @@ export default function Notes({ data }) {
               </div>
             </div>
 
-            <textarea defaultValue={data.notes}></textarea>
+            <textarea 
+            defaultValue={data.notes}
+            onChange={e => setChangedNote(e.target.value)}
+            onBlur={e => handleSave(e.target, data.notes)}
+            >
+
+            </textarea>
             <span>
               <AiOutlineExclamationCircle size="20"/>
             </span>
